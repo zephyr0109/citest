68de21b02b21eb05c7f4b8348e59be2e210f64d8
junyul.im <junyul.im@homeplus.co.kr>
2025-12-02 12:53:58 +0900

    테스트 소스, 웹 소스 수정

--- src/test/java/com/example/hello/HelloAppTest.java
+++ src/test/java/com/example/hello/HelloAppTest.java
@@ -1,14 +1,16 @@
 package com.example.hello;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloAppTest {
 
     @Test
     void testGreet() {
         HelloApp app = new HelloApp();
+        assertEquals("Hello CI/CD! - latest", app.greet());
         System.out.println("test done");
-        assertEquals("Hello CI/CD!", app.greet());
+
     }
 }
