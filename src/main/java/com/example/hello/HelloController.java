494a3921a314c986a5c6c5ca56257254f7f7fd02
junyul.im <junyul.im@homeplus.co.kr>
2025-12-04 11:20:31 +0900

    PR request 처리 테스트

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello CI Web! - development";
+        return "Hello CI Web! - development, PRTest";
     }
 }
